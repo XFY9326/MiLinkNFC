b3873c610a55e724cfefcc9361e6ee2789d086e1
XFY9326 <xfy9326@qq.com>
2023-12-16 12:23:27 +0800

    Add more app version check

--- app/src/main/java/tool/xfy9326/milink/nfc/utils/MiContinuityUtils.kt
+++ app/src/main/java/tool/xfy9326/milink/nfc/utils/MiContinuityUtils.kt
@@ -6,16 +6,22 @@ import tool.xfy9326.milink.nfc.data.PackageData
 object MiContinuityUtils {
     private const val PKG_MI_LINK_SERVICE = "com.milink.service"
     private const val PKG_MI_CONNECT_SERVICE = "com.xiaomi.mi_connect_service"
+    private const val PKG_MIRROR = "com.xiaomi.mirror"
+    private const val PKG_FILE_EXPLORER = "com.android.fileexplorer"
+    private const val PKG_GALLERY = "com.miui.gallery"
 
     private const val PKG_META_DATA_XIAOMI_CONTINUITY_VERSION_NAME = "com.xiaomi.continuity.VERSION_NAME"
 
-    private val ONE_HOP_RELATED_PACKAGE_NAMES = arrayOf(
-        PKG_MI_LINK_SERVICE,
+    private val ONE_TAP_RELATED_PACKAGE_NAMES = arrayOf(
         PKG_MI_CONNECT_SERVICE,
+        PKG_MI_LINK_SERVICE,
+        PKG_MIRROR,
+        PKG_FILE_EXPLORER,
+        PKG_GALLERY
     )
 
     fun getNfcRelatedPackageDataMap(context: Context): Map<String, PackageData?> =
-        ONE_HOP_RELATED_PACKAGE_NAMES.associateWith { context.getPackageData(it) }
+        ONE_TAP_RELATED_PACKAGE_NAMES.associateWith { context.getPackageData(it) }
 
     fun isLocalDeviceSupportLyra(context: Context): Boolean {
         return context.getPackageMetaData(PKG_MI_CONNECT_SERVICE)?.getString(PKG_META_DATA_XIAOMI_CONTINUITY_VERSION_NAME) != null
