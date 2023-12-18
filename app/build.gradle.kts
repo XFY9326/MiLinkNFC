f15a4e73bd1957815538f9722c3e022d92fc07ea
XFY9326 <xfy9326@qq.com>
2023-12-18 18:00:48 +0800

    Update v1.8.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "tool.xfy9326.milink.nfc"
         minSdk = 29
         targetSdk = 34
-        versionCode = 10
-        versionName = "1.8.1"
+        versionCode = 11
+        versionName = "1.8.2"
 
         resourceConfigurations += "zh"
 
